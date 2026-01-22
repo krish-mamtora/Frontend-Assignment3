d39b3ae1c5dba251ef09e60bcef34217a4c3943d
krish-mamtora <krish.mamtora@roimaint.com>
2026-01-22 17:49:08 +0530

    usequery used for api

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,16 +1,63 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../App.css'
-import {Navbar} from '../components/Navbar'
+import axios from 'axios';
+import './ProductList.css'
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
+
+async function fetchProducts() {
+    const res = await fetch("https://fakestoreapi.com/products/");
+    if (!res.ok) throw new Error("Network response error");
+    return res.json();
+}
 
 function ProductList() {
 
-    
+    // const [products , setProducts] = useState([]);
+    // useEffect(()=>{
+    //     axios.get('https://fakestoreapi.com/products/')
+    //     .then(response=>setProducts(response.data))
+    //     .catch(error => console.log(error));
+    // },[])
 
-  return (
-    <>
-    <h2>ProductList</h2>
-    </>
-  )
+    const { data, error, isLoading } = useQuery({
+        queryKey: ["products"], 
+        queryFn: fetchProducts,
+    });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+    return (
+        <>
+        <h2>ProductList</h2>
+        <div className='productlist'>
+        {
+            data.map((product , index)=>(
+                <div className='productcard'>
+                {product && Object.keys(product).map((title , i)=>{
+                        // console.log(title ,',', product[title]);
+                        return (
+                            <div key={i} className=''>
+                                
+                                {title && title=="image" && (
+                                    <>
+                                    <img style={{height:"50px" , width:"50px"}} src={product[title]} alt="img" /></>
+                                )}
+
+                                {title && (title=="title" || title=="price"
+                                     || title=="category") &&(
+                                         <> <span>{title}</span> : <span>{String(product[title])}</span></>
+                                     )}
+                                
+                                     
+                            </div>
+                        )
+                    })
+                }
+                </div>
+            ))
+        }</div>
+        </>
+    )
 }
 
 export default ProductList
--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,15 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './pages/ErrorPage.tsx';
 import Home from './pages/Home.tsx';
 import About from './pages/About.tsx';
-import Products from './pages/Products.tsx';
 import Cart from './pages/Cart.tsx';
 import ProductList from './components/ProductList.tsx';
 import ProductDetails from './components/ProductDetails.tsx';
 import ProductCustomize from './components/ProductCustomize.tsx'
 import Shop from './pages/Shop.tsx';
+import axios from 'axios';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
     path: "/",
@@ -23,9 +25,8 @@ const router = createBrowserRouter([
       {index : true, element : <Home />},
       {path : "cart" , element : <Cart/>},
       {path : "about" , element : <About/>},
-      {path : "shop" , element : <Shop/>,
+      {path : "shop" , element : <ProductList />,
         children : [
-            {   index :  true, element : <ProductList />} ,        
              {   path : "productID" , element : <ProductDetails/>,
                 children : [
                   {
@@ -41,6 +42,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router}></RouterProvider>
-  </StrictMode>,
+    </QueryClientProvider>
+  </StrictMode>
 )
